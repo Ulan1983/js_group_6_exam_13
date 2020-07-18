6e48108c79b2ecc1f720c22625bc6f34f6a4f1aa
Ulan Beltaev <ulanbeltaev@gmail.com>
2020-07-18 09:48:02 +0600

    Добавил на бэке файлы конфигураций, фикстур, index и multer

---
+++ backend/config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const rootPath = __dirname;
+
+const env = process.env.NODE_ENV;
+
+let database = 'mongodb://localhost/exam_13';
+let port = 8000;
+
+if (env === 'test') {
+	database = 'mongodb://localhost/exam_13-test';
+	port = 8010;
+}
+
+module.exports = {
+	rootPath,
+	userAvatar: path.join(rootPath, 'public/uploads/userAvatar'),
+	locationImage: path.join(rootPath, 'public/uploads/locationImage'),
+	database,
+	databaseOptions: {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	},
+	port,
+};
